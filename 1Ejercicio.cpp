92ddb39dbaf663b95992fdfa70c3f68ca322879e
Carlostorres010 <ctorresv@unjbg.edu.pe>
2024-06-18 22:45:03 -0500

    Se ingreso texto adicional cerca de las variables

--- 1Ejercicio.cpp
+++ 1Ejercicio.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main() {
     int X;
 
-    cout << "Ingrese su numero: "; cin >>X;
+    cout << "Ingrese su numero: "; cin >>X;/*Esta es la primera variable entero*/
 
     while (X >= 0) {
-        cout << "El numero que escogido es: " << X<< endl;
+        cout << "El numero que escogido es: " << X<< endl;/*Esta es la segunda variable entero que ingreso*/
         
         cout << "Ingrese otro numero (un numero negativo para que finalize el programa): "; cin >> X;
     }
